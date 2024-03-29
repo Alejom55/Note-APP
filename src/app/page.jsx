4386d988abe466a689fb7d459d7df4c3acde8779
vel <ospinadaniel142@gmail.com>
2024-03-29 16:15:50 -0500

    Creación de la homePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,16 @@
-export default function Home() {
-  return (
-    <h1>Home Page</h1>
-  );
-}
+import "./homepage.css"
+import Link from 'next/link';
+
+export default function HomePage() {
+    return (
+      <div className="container">
+        <header className="header">
+        <div className="links-container">
+        <Link href = "/taskPage" className="link"><h1 className="h1" >Log In</h1></Link>  
+        <Link href = "/register" className="link"><h1 className="h1" >Sing Up</h1></Link> 
+        </div>
+        </header>
+        
+        </div> 
+    );
+  };
---
+++ src/app/taskPage/page.jsx
@@ -0,0 +1,15 @@
+import styles from './taskPage.css';
+
+
+
+const taskPage = () => {
+    return (
+      
+        <div className='background'>
+          <header className='header'> <h1 className='h1'>Lista de Tareas</h1></header>
+        </div>
+    );
+  };
+  
+  export default taskPage;
+
