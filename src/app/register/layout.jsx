695246956f2504d16229caa0e065aa1728b6af5b
alejom55 <alejandromarin203@gmail.com>
2024-04-01 02:19:31 -0500

    Eliminar contenido de la rama

--- src/app/register/layout.jsx
+++
@@ -1,9 +0,0 @@
-import RedirectAuth from "@/components/redirectAuth";
-export default async function RegisterLayout({ children }) {
-    await RedirectAuth();
-    return (
-        <div>
-            {children}
-        </div>
-    );
-}
