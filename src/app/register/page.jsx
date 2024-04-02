a330524dd1ebbb7de7c569975b623e786d0a1771
Daniel Lasso <dlassoc123@gmail.com>
2024-04-01 22:14:49 -0500

    css de registro terminado

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -3,96 +3,116 @@ import React, { useState } from 'react';
 import axios, { AxiosError } from 'axios';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Importar los iconos de ojo
 import { FaClipboardList } from "react-icons/fa6";
+import { IoExitOutline } from "react-icons/io5";
+
 import './register.css';
-function SignUp() {
-    const [error, setError] = useState();
+const ExitButton = () => {
     const router = useRouter();
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-        const password = formData.get('password');
-        const confirmPassword = formData.get('confirmPassword');
-
-        if (password !== confirmPassword) {
-            setError('Las contraseñas no coinciden');
-            return;
-        }
-
-        try {
-            const res = await axios.post('/api/auth/signup', {
-                email: formData.get('email'),
-                password: formData.get('password'),
-                fullname: formData.get('fullname')
-            });
-
-            const restSignIn = await signIn('credentials', {
-                email: res.data.email,
-                password: formData.get('password'),
-                redirect: false,
-            })
-            if (restSignIn.ok) {
-                router.push('/dashboard');
-            }
-        } catch (error) {
-            console.log(error);
-            if (error instanceof AxiosError) {
-                setError(error.response?.data.message);
-            }
-        }
+  
+    const handleExit = () => {
+      router.push("/");
     };
-
+  
     return (
-        
-        <div className="register-container">
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <div className="input-wrapper">
-                <h1>Registrarse</h1>
-                <h2>Gestor</h2>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <input
-                            type="text"
-                            name="fullname"
-                            placeholder='Nombre completo'
-                            required
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder='Correo electrónico'
-                            required
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder='Contraseña'
-                            required
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="password"
-                            id="confirmPassword"
-                            name="confirmPassword"
-                            placeholder='Confirmar Contraseña'
-                            required
-                        />
-                    </div>
-                    <button type="submit">Registrarse</button>
-                </form>
-            </div>
-            <FaClipboardList className="clipboard-icon" />
-        </div>
+      <button className="exit-button" onClick={handleExit}>
+        <IoExitOutline className="exit-icon" />
+      </button>
     );
-    
-}
-
-export default SignUp;
+  };
+  
+  function SignUp() {
+    const [error, setError] = useState();
+    const [showPassword, setShowPassword] = useState(false);
+    const router = useRouter();
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      const password = formData.get('password');
+      const confirmPassword = formData.get('confirmPassword');
+  
+      if (password !== confirmPassword) {
+        setError('Las contraseñas no coinciden');
+        return;
+      }
+  
+      try {
+        const res = await axios.post('/api/auth/signup', {
+          email: formData.get('email'),
+          password: formData.get('password'),
+          fullname: formData.get('fullname')
+        });
+  
+        const restSignIn = await signIn('credentials', {
+          email: res.data.email,
+          password: formData.get('password'),
+          redirect: false,
+        });
+  
+        if (restSignIn.ok) {
+          router.push('/dashboard');
+        }
+      } catch (error) {
+        console.log(error);
+        if (error instanceof AxiosError) {
+          setError(error.response?.data.message);
+        }
+      }
+    };
+  
+    return (
+      <div className="register-container">
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <ExitButton />
+        <div className="input-wrapper">
+          <h1>Registrate en nuestro</h1>
+          <h2>gestor de notas</h2>
+          <FaClipboardList className="clipboard-icon" />
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <input
+                type="text"
+                name="fullname"
+                placeholder='Nombre completo'
+                required
+              />
+            </div>
+            <div className="input-group">
+              <input
+                type="email"
+                name="email"
+                placeholder='Correo electrónico'
+                required
+              />
+            </div>
+            <div className="input-group">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                name="password"
+                placeholder='Contraseña'
+                required
+              />
+              <button type="button" className="eye-button" onClick={() => setShowPassword(!showPassword)}>
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+            <div className="input-group">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id="confirmPassword"
+                name="confirmPassword"
+                placeholder='Confirmar Contraseña'
+                required
+              />
+            </div>
+            <button type="submit">Registrarse</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+  
+  export default SignUp;
