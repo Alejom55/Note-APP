485fc61d68fef2bdc9e76899b0d9f8b2b16e372d
Daniel Lasso <dlassoc123@gmail.com>
2024-04-03 12:02:19 -0500

    ahora si todo terminado, faltaba una cosita

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,23 +3,25 @@
   import Link from 'next/link';
   import RedirectAuth from "@/components/redirectAuth";
   import { MdOutlinePersonAddAlt } from "react-icons/md";
-  import { GiJoin } from "react-icons/gi";
+  import { AiOutlineEnter } from "react-icons/ai";
   
   export default async function HomePage() {
     await RedirectAuth();
-    return (
+      return (
       <div className="container">
         <div className="header"></div>
         <div className="links-container">
           <h1 className="h1" ><Link href="/login" className="link">Iniciar Sesion</Link></h1>
-          <GiJoin className="joinicon"/>
+          <AiOutlineEnter  className="joinicon"/>
   
           <h1 className="h1" ><Link href="/register" className="linkregister">Registrarse </Link></h1>
           <MdOutlinePersonAddAlt className="ADDIcon"/>
   
         </div>
-        <div className= "titulopagina">
-          <h1>Gestor<br />de<br />Tareas</h1>
+        <div className="titulopagina">
+          <div className="titulo-container">
+            <h1>Gestor<br />de<br />Tareas</h1>
+          </div>
         </div>
       </div>
     );
