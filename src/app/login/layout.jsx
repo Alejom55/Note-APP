96d8118055feaac119f0bada8e51271544952826
alejom55 <alejandromarin203@gmail.com>
2024-06-12 02:10:44 -0500

    chore: Update dashboard layout, add middleware, and implement new functionality

--- src/app/login/layout.jsx
+++
@@ -1,9 +0,0 @@
-import RedirectAuth from "@/components/redirectAuth";
-export default async function LogInLayout({ children }) {
-    await RedirectAuth();
-    return (
-        <div>
-            {children}
-        </div>
-    );
-}
