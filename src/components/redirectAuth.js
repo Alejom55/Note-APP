0bcd095da42a48c6cb55ce7d80fc55cc7a17b81d
alejom55 <alejandromarin203@gmail.com>
2024-06-12 02:23:40 -0500

    chore: Update redirectAuth.js to handle server session and redirect to dashboard if session exists

--- src/components/redirectAuth.js
+++ src/components/redirectAuth.js
@@ -1,15 +1,13 @@
 // 'use client'
 import { getServerSession } from 'next-auth';
 import { redirect } from "next/navigation";
-
+import { handlerAuth } from '@/app/api/auth/[...nextauth]/route';
 async function RedirectAuth() {
 
-    const session = await getServerSession();
-    // if (session) {
-    //     redirect('/dashboard');;
-    // }
-
-
+    const session = await getServerSession(handlerAuth);
+    if (session) {
+        redirect('/dashboard');;
+    }
 }
 
 export default RedirectAuth;
